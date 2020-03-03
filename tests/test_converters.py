849f2624cad0c3cbf87a9840987169662fd40061
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2020-03-03 14:26:13 +0200

    Updated tests

--- tests/test_converters.py
+++ tests/test_converters.py
@@ -10,6 +10,10 @@ import pathlib
 from typing import List  # pylint: disable=W0611
 
 from django.test import TestCase
+import git
+from po2xls.converters import PoToXls
+import polib
+import xlrd
 
 from xls2po.converters import XlsToPo
 from xls2po.exceptions import ConversionError
@@ -44,6 +48,10 @@ class XlsToPoTest(TestCase):
 
         os.remove("xls2po/locale/en/LC_MESSAGES/django.xls")
         os.remove("xls2po/locale/uk/LC_MESSAGES/django.xls")
+
+        # reset git repo state to avoid commit changed .po files
+        repo = git.Repo(".")
+        repo.git.reset("--hard")
 
         super().tearDownClass()
 
@@ -80,4 +88,41 @@ class XlsToPoTest(TestCase):
             expr=pathlib.Path("xls2po/locale/uk/LC_MESSAGES/django.po").exists()
         )
 
-    # TODO: add check file content test.
+    def test_convert(self):
+        """
+        convert method must write converted data to .xls file.
+        """
+
+        XlsToPo(src="xls2po/locale/uk/LC_MESSAGES/django.xls").convert()
+
+        po = polib.pofile(
+            pofile="xls2po/locale/uk/LC_MESSAGES/django.po"
+        )  # type: polib.POFile
+        po_metadata = [["key", "value"]] + [
+            [data, po.metadata[data]] for data in po.metadata
+        ]  # type: List[List[str]]
+        po_strings = [["msgid", "msgstr"]] + [
+            [entry.msgid, entry.msgstr] for entry in po
+        ]  # type: List[List[str]]
+        xls = xlrd.open_workbook(
+            filename="xls2po/locale/uk/LC_MESSAGES/django.xls"
+        )  # type: xlrd.Workbook
+        xls_metadata = [
+            xls.sheet_by_name(sheet_name=PoToXls.METADATA_SHEET_NAME).row_values(
+                rowx=row_i
+            )
+            for row_i in range(
+                0, xls.sheet_by_name(sheet_name=PoToXls.METADATA_SHEET_NAME).nrows
+            )
+        ]  # type: List[List[str]]
+        xls_strings = [
+            xls.sheet_by_name(sheet_name=PoToXls.STRINGS_SHEET_NAME).row_values(
+                rowx=row_i
+            )
+            for row_i in range(
+                0, xls.sheet_by_name(sheet_name=PoToXls.STRINGS_SHEET_NAME).nrows
+            )
+        ]  # type: List[List[str]]
+
+        self.assertListEqual(list1=po_metadata, list2=xls_metadata)
+        self.assertListEqual(list1=po_strings, list2=xls_strings)
